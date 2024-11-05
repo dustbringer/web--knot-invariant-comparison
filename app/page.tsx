0d000cd70ade0c1beee094ec314b95434d944f71
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2024-11-05 19:03:32 +1100

    Working plots; github pages test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,18 @@ import jonesBM from "./data/jones-bm.json";
 import a2BM from "./data/a2-bm.json";
 import khovanovBM from "./data/khovanov-bm.json";
 import percentUnique from "./data/percent-unique.json";
+import randomPairs from "./data/random-pairs.json";
 
 const plotDataUnique = {
   name: "%Unique",
   json: percentUnique,
-  docid: "abd3f26e-9d72-41e4-8bad-f731fa26442b",
+  docid: "c2ab9205-16c1-4379-bf69-4291d1d1124f",
+};
+
+const plotDataRandomPairs = {
+  name: "Random Pairs",
+  json: randomPairs,
+  docid: "2409df14-78b7-4d37-91f3-4f3beff566a3",
 };
 
 const plotDataBM = [
@@ -42,58 +49,53 @@ const plotDataBM = [
 export default function Home() {
   const [plot, setPlot] = React.useState(0);
 
-  const onChange = (e) => {
-    setPlot(e.target.value);
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPlot(+e.target.value);
   };
 
-  React.useEffect(() => {
-    // Clear nodes
-    const ids = ["bokeh-plot-unique"];
-    ids.forEach((id) => {
-      document.getElementById(id)?.replaceChildren();
-    });
-
-    // Dynamically import to avoid "document doesn't exist" in prerender
-    import("./bokeh/bokeh-widgets.esm.min.js")
-      .then(() => import("./bokeh/bokeh.esm.min.js"))
-      .then((res) => {
-        const Bokeh = res.default;
-        Bokeh.embed.embed_items(JSON.stringify(plotDataUnique.json), [
-          {
-            docid: plotDataUnique.docid,
-            roots: { x: "bokeh-plot-unique" },
-            root_ids: ["x"],
-          },
-        ]);
-      });
-  }, []);
-
-  React.useEffect(() => {
+  const showPlot = (
+    data: { json: object; docid: string },
+    className: string
+  ) => {
     // source: bokeh html output
     // source for inspiration https://discourse.bokeh.org/t/bokehjs-frontend-library-react-recommended-approach/8919
 
     // const myNode = document.getElementById("bokeh_plot");
     // myNode.replaceChildren(); // clean previous plot
 
-    // Clear nodes
-    const ids = ["bokeh-plot-bm"];
-    ids.forEach((id) => {
-      document.getElementById(id)?.replaceChildren();
-    });
+    document.getElementById(className)?.replaceChildren();
 
     // Dynamically import to avoid "document doesn't exist" in prerender
     import("./bokeh/bokeh-widgets.esm.min.js")
       .then(() => import("./bokeh/bokeh.esm.min.js"))
       .then((res) => {
         const Bokeh = res.default;
-        Bokeh.embed.embed_items(JSON.stringify(plotDataBM[plot].json), [
+        Bokeh.embed.embed_items(JSON.stringify(data.json), [
           {
-            docid: plotDataBM[plot].docid,
-            roots: { x: "bokeh-plot-bm" },
+            docid: data.docid,
+            roots: { x: className },
             root_ids: ["x"],
           },
         ]);
       });
+  };
+
+  React.useEffect(() => {
+    const data = plotDataUnique;
+    const className = "bokeh-plot-unique";
+    showPlot(data, className);
+  }, []);
+
+  React.useEffect(() => {
+    const data = plotDataRandomPairs;
+    const className = "bokeh-plot-random-pairs";
+    showPlot(data, className);
+  }, []);
+
+  React.useEffect(() => {
+    const data = plotDataBM[plot];
+    const className = "bokeh-plot-bm";
+    showPlot(data, className);
   }, [plot]);
 
   return (
@@ -101,6 +103,11 @@ export default function Home() {
       <div className="section">
         <h1>Percentage of Unique Values</h1>
         <div className="bokeh-plot" id="bokeh-plot-unique" />
+      </div>
+
+      <div className="section">
+        <h1>Comparisons of Random Pairs until equal</h1>
+        <div className="bokeh-plot" id="bokeh-plot-random-pairs" />
       </div>
 
       <div className="section">
