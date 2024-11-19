2f39af37be2d54153ee18c4bfc5a6e8483f97c87
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2024-11-20 10:53:49 +1100

    Edited config for deploy

--- next.config.ts
+++ next.config.ts
@@ -4,29 +4,29 @@ import type { NextConfig } from "next";
 // BUILD: uncomment
 const nextConfig: NextConfig = {
   // // from https://github.com/gregrickaby/nextjs-github-pages
-  // /**
-  //  * Enable static exports for the App Router.
-  //  *
-  //  * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-  //  */
-  // output: "export",
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
 
-  // /**
-  //  * Set base path. This is the slug of your GitHub repository.
-  //  *
-  //  * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-  //  */
-  // basePath: "/build",
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/build",
 
-  // /**
-  //  * Disable server-based image optimization. Next.js does not support
-  //  * dynamic features with static exports.
-  //  *
-  //  * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-  //  */
-  // images: {
-  //   unoptimized: true,
-  // },
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
