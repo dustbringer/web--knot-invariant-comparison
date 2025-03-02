b323b2d141a47a5f11702c385ebf128c9022e051
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2025-03-03 00:22:53 +1100

    Config for build

--- next.config.ts
+++ next.config.ts
@@ -9,22 +9,22 @@ const nextConfig: NextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: "/web--knot-invariant-comparison",
-  // // from https://github.com/gregrickaby/nextjs-github-pages
-  // /**
-  //  * Enable static exports for the App Router.
-  //  *
-  //  * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-  //  */
-  // output: "export",
-  // /**
-  //  * Disable server-based image optimization. Next.js does not support
-  //  * dynamic features with static exports.
-  //  *
-  //  * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-  //  */
-  // images: {
-  //   unoptimized: true,
-  // },
+  // from https://github.com/gregrickaby/nextjs-github-pages
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
